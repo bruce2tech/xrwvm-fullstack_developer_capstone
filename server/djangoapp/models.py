0837d367b46ec5b0df9054faad0a303edb93c688
bruce2tech <pbruce.resume@gmail.com>
2025-01-23 23:32:23 -0500

    more lint fixes on the python files

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -8,7 +8,7 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    
+
     def __str__(self):
         return self.name  # Return the name as the string representation
 
@@ -33,9 +33,9 @@ class CarModel(models.Model):
     year = models.IntegerField(
                                 default=2023,
                                 validators=[
-                                MaxValueValidator(2023),
-                                MinValueValidator(2015)
-                              ])
+                                            MaxValueValidator(2023),
+                                            MinValueValidator(2015)
+                                            ])
     # Other fields as needed
 
     def __str__(self):
